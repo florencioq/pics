60a45113e2443c4d0e210a22ddd1dc30aab5e3f3
Florencio <florencio@ideos.com.br>
2022-06-25 17:23:07 -0300

    format

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,26 +1,30 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-    onInputChange(event) {
-        console.log(event.target.value);
-    }
+  state = { term: "" };
 
-    onInputClick() {
-        console.log("input was clicked");
-    }
+  componentDidUpdate() {
+    console.log(this.state.term);
+  }
 
-    render() {
-        return (
-            <div className="ui segment">
-                <form className="ui form">
-                    <div className="field">
-                        <label>Image Search</label>
-                        <input type="text" onChange={this.onInputChange}/>
-                    </div>
-                </form>
-            </div>
-        );
-    };
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) =>
+                this.setState({ term: e.target.value.toUpperCase() })
+              }
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
