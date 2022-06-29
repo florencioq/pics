4f498fb1a20bde82884bf80de07f51ca0f1db174
Florencio <florencio@ideos.com.br>
2022-06-29 08:59:05 -0300

    Fazendo chamada async ao unslash

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,16 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
-      params: { query: term },
-      headers: {
-        Authorization: "Client-ID -0xwAAoF3bHUHm1X1SOKrZOu-ikleJeX6SUCe3pZB_A",
-      },
-    });
+  async onSearchSubmit(term) {
+    const response = await axios
+      .get("https://api.unsplash.com/search/photos", {
+        params: { query: term },
+        headers: {
+          Authorization:
+            "Client-ID -0xwAAoF3bHUHm1X1SOKrZOu-ikleJeX6SUCe3pZB_A",
+        },
+      });
+      console.log(response.data.results);
   }
 
   render() {
