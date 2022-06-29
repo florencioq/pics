bc1ce552a850645784c0de1be7f1a89088a81f5c
Florencio <florencio@ideos.com.br>
2022-06-29 08:50:07 -0300

    fazendo uma chamada ao site unslash.com

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  onSearchSubmit(term) {
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID -0xwAAoF3bHUHm1X1SOKrZOu-ikleJeX6SUCe3pZB_A",
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
